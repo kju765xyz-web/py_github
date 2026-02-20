7efd27cb32f373d591b1154f815144c9ae1f1066
jjf <kju765xyz@gmail.com>
2026-02-20 14:52:25 +0900

    changing color plot

--- test.py
+++ test.py
@@ -5,9 +5,10 @@ import numpy as np
 x = np.linspace(0, 10, 100)
 y = np.sin(x)
 
-plt.plot(x, y)
+plt.plot(x, y, label='sin(x)', color='red')
 plt.title('Sine Wave')
 plt.xlabel('x')
 plt.ylabel('sin(x)')
 plt.grid()
+plt.legend()
 plt.show()
